f5123cded405e42cfaaffb33c6f4cccc9d654972
jiyinyiyong <jiyinyiyong@gmail.com>
2020-11-02 17:25:46 +0800

    working(but ugly) prototype

--- README.md
+++ README.md
@@ -1,47 +1,12 @@
 
-Calcit Workflow
+Timegrass Viewer
 ----
 
-> Personal project template based on ClojureScript, Respo, shadow-cljs, Cirru.
+> View data from timegrass
 
 ### Usage
 
-Make sure you have Node.js and JVM installed. Then install dependencies:
-
-```bash
-yarn
-```
-
-All build script are running in:
-
-```bash
-yarn watch # to start watch server...
-yarn page # to generate target/index.html file
-yarn ln # link CSS files to target/ folder
-yarn build # to build release bundles, with local assets links
-```
-
-Edit `config.cljs` to change the settings as you want(by default it sends assets to my server). To release:
-
-```bash
-yarn build-cdn # to build release bundles, with assets link to CDN
-yarn upload # to upload assets to my server
-```
-
-Edit Clojure with [calcit-editor](https://github.com/Cirru/calcit-editor):
-
-```bash
-npm i -g calcit-editor
-calcit-editor # watching server...
-
-op=compile calcit-editor # if you need to generate code at once
-```
-
-Package jar file and send to Clojars with [deps-library](https://github.com/applied-science/deps-library):
-
-```bash
-clj -A:release
-```
+*TODO*
 
 ### Workflow
 
--- src/app/comp/container.cljs
+++ src/app/comp/container.cljs
@@ -2,11 +2,64 @@
 (ns app.comp.container
   (:require [hsl.core :refer [hsl]]
             [respo-ui.core :as ui]
-            [respo.core :refer [defcomp defeffect <> >> div button textarea span input]]
+            [respo.core
+             :refer
+             [defcomp defeffect <> >> list-> div button textarea span input]]
             [respo.comp.space :refer [=<]]
             [reel.comp.reel :refer [comp-reel]]
             [respo-md.comp.md :refer [comp-md]]
-            [app.config :refer [dev?]]))
+            [app.config :refer [dev?]]
+            [cljs.reader :refer [read-string]]
+            ["dayjs" :as dayjs]))
+
+(def style-list {:padding-left 40})
+
+(defcomp
+ comp-viewer
+ (data)
+ (let [tasks (vals (merge (:working-tasks data) (:finished-tasks data)))
+       notes (vals (:notes data))
+       all-time (filter some? (concat (map :created-time tasks) (map :time notes)))
+       from-day (dayjs (apply min all-time))
+       to-day (dayjs (apply max all-time))
+       days (js/Math.ceil
+             (/ (- (apply max all-time) (apply min all-time)) (* 1000 60 60 24)))]
+   (div
+    {}
+    (list->
+     {}
+     (->> (range (inc days))
+          (map
+           (fn [idx]
+             (let [the-day (.add from-day idx "days")]
+               [idx
+                (let [day-format (.format the-day "YYYY-MM-DD")
+                      day-tasks (->> tasks
+                                     (filter
+                                      (fn [task]
+                                        (=
+                                         day-format
+                                         (.format (dayjs (:created-time task)) "YYYY-MM-DD")))))
+                      day-notes (->> notes
+                                     (filter
+                                      (fn [task]
+                                        (=
+                                         day-format
+                                         (.format (dayjs (:time task)) "YYYY-MM-DD")))))]
+                  (div
+                   {}
+                   (<> day-format {:font-family ui/font-fancy, :color (hsl 0 0 80)})
+                   (if (not (empty? day-tasks))
+                     (list->
+                      {:style style-list}
+                      (->> day-tasks
+                           (map-indexed (fn [idx task] [idx (div {} (<> (:text task)))])))))
+                   (=< nil 20)
+                   (if (not (empty? day-notes))
+                     (list->
+                      {:style style-list}
+                      (->> day-notes
+                           (map-indexed (fn [idx note] [idx (div {} (<> (:text note)))])))))))]))))))))
 
 (defcomp
  comp-container
@@ -14,21 +67,34 @@
  (let [store (:store reel)
        states (:states store)
        cursor (or (:cursor states) [])
-       state (or (:data states) {:content ""})]
+       state (or (:data states) {:content "", :list? false, :data? nil})]
    (div
-    {:style (merge ui/global ui/row)}
-    (textarea
-     {:value (:content state),
-      :placeholder "Content",
-      :style (merge ui/expand ui/textarea {:height 320}),
-      :on-input (fn [e d!] (d! cursor (assoc state :content (:value e))))})
-    (=< 8 nil)
-    (div
-     {:style ui/expand}
-     (comp-md "This is some content with `code`")
-     (=< "8px" nil)
-     (button
-      {:style ui/button,
-       :inner-text "Run",
-       :on-click (fn [e d!] (println (:content state)))}))
-    (when dev? (comp-reel (>> states :reel) reel {})))))
+    {:style (merge ui/fullscreen ui/column)}
+    (if (:list? state)
+      (div
+       {:style ui/expand}
+       (div
+        {}
+        (button
+         {:style ui/button,
+          :inner-text "Edit",
+          :on-click (fn [e d!] (d! cursor (update state :list? not)))})
+        (comp-viewer (:data state))))
+      (div
+       {:style (merge ui/global ui/expand ui/column)}
+       (div
+        {:style (merge {:padding 8})}
+        (button
+         {:style ui/button,
+          :inner-text "Read",
+          :on-click (fn [e d!]
+            (d!
+             cursor
+             (-> state (update :list? not) (assoc :data (read-string (:content state))))))}))
+       (textarea
+        {:value (:content state),
+         :placeholder "Content",
+         :style (merge ui/expand ui/textarea {:height 420}),
+         :on-input (fn [e d!] (d! cursor (assoc state :content (:value e))))})
+       (=< 8 nil)
+       (when dev? (comp-reel (>> states :reel) reel {})))))))
